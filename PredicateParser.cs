da14de02f3cee6bc34cde8868ba8b3592bb6c77d
faridkhalikov <fkhalikov@gmail.com>
2019-07-02 12:08:31 +0100

    Initial commit

---
+++ PredicateParser.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace RegexPredicateParser
+{
+    public class PredicateParser<T>
+        where T: class
+    {
+        static string regexParanthesisString = @"(?<paranthesis>\w*\W*=\W*[\w\d])";
+        static string regexExpressionString =
+            @"(?<leftexp>[\s\w]+)(?<operator>(={2}|<=|>=|<|>){1})\s*(?<rightexp>[\'\w\d]+)";
+        static string regexOperatorString = @"(?<operator>([\=\s\w]+((\|{2})|(\&{2}))?))";
+
+        Regex parantethisRegex = new Regex(regexParanthesisString);
+        Regex expressionRegex = new Regex(regexExpressionString);
+        public Expression Parse(string query)
+        {
+            var match = expressionRegex.Match(query);
+
+            if (!match.Success)
+                throw new Exception($"Failed to parse the query {query}");
+
+            var leftExp = match.Groups["leftexp"].Value;
+            var rightExp = match.Groups["rightexp"].Value;
+            var operatorStr = match.Groups["operator"].Value;
+
+            if (string.IsNullOrWhiteSpace(leftExp))
+            {
+                throw new Exception(
+                   $"Left expression can not be null or empty in expression '{query}'.");
+            }
+            
+            if (string.IsNullOrWhiteSpace(rightExp))
+            {
+                throw new Exception(
+                   $"Right expression can not be null or empty in expression '{query}'.");
+            }
+
+            leftExp = leftExp.Trim();
+            rightExp = rightExp.Trim();
+
+            var pe = Expression.Parameter(typeof(T),"t");
+
+            Expression right = Expression.Constant(rightExp);
+            Expression left = Expression.Property(pe, typeof(T).GetProperty(leftExp));
+
+            return ApplyOperator(operatorStr, left, right);
+        }
+
+        public Expression ApplyOperator(
+            string operatorStr,
+            Expression left,
+            Expression right)
+        {
+            if (string.IsNullOrWhiteSpace(operatorStr))
+                throw new Exception("Operator can not be empty or null.");
+
+            operatorStr = operatorStr.Trim();
+
+            if (operatorStr == "==")
+            {
+                return Expression.Equal(left, right);
+            }
+            else if (operatorStr == "<=")
+            {
+                throw new NotImplementedException();
+            }
+            else if (operatorStr == ">=")
+            {
+                throw new NotImplementedException();
+            }
+            else if (operatorStr == ">")
+            {
+                throw new NotImplementedException();
+            }
+            else if (operatorStr == "<")
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                throw new Exception($"Invalid operator {operatorStr}.");
+            }
+        }
+
+        public bool HasParanthesis(string query)
+        {
+            return parantethisRegex.IsMatch(query);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using Xunit;
+using System.Linq.Dynamic;
+
+namespace RegexPredicateParser
+{
+    class Program
+    {
+        static PredicateParser<Person> pParser = new PredicateParser<Person>();
+
+        static void Main(string[] args)
+        {
+            //TestSingleComparisonStringEquality();
+            TestDynamicQuerable();
+        }
+
+        public static void TestSingleComparisonStringEquality()
+        {
+            string text = "Name == A1";
+
+            var people = GetPeople().AsQueryable();
+
+            var pe = Expression.Parameter(
+                typeof(Person),"person");
+
+            Expression exp = pParser.Parse(text);
+
+            var whereCallExpression = Expression.Call(
+              typeof(Queryable),
+              "Where",
+              new[] { typeof(Person)},
+              people.Expression,
+              Expression.Lambda<Func<Person,bool>>(
+                  exp, pe));
+
+            var result = people.Provider.CreateQuery<Person>(
+                   whereCallExpression).ToList();
+
+            Assert.Single(result);
+        }
+
+        public static void TestDynamicQuerable()
+        {
+        
+            var people = GetPeople()
+                .AsQueryable()
+                .Where(@"(Name == ""N1"" || Surname == ""S3"")
+                && (Height > 0.2 || Age < 3) 
+                || Address.Address1==""A5""
+                || Address.Address1==""A6""")
+                .ToList();
+
+            Assert.Equal(4, people.Count);
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public Address Address { get; set; }
+            public string Postcode { get; set; }
+            public int Age { get; set; }
+            public double Height { get; set; }
+        }
+
+        public class Address
+        {
+            public string Address1 { get; set; }
+            public string Postcode { get; set; }
+
+        }
+
+        private static List<Person> GetPeople()
+        {
+            var people = new List<Person>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                people.Add(new Person
+                {
+                    Name = $"N{i}",
+                    Surname = $"S{i}",
+                    Address = new Address() {
+                        Address1 = $"A{i}",
+                        Postcode = $"P{i}"
+                    },
+                    Age = i,
+                    Height = i * 0.2d
+                });
+            }
+
+            return people;
+        }
+    }
+}
